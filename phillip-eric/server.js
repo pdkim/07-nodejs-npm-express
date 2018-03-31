c2abab60a3931e2c2d08151822506615bfa2ecb1
Phillip Kim <phildkim@gmail.com>
2018-03-31 10:15:09 -0700

    won't merge until commited?'

--- phillip-eric/server.js
+++ phillip-eric/server.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log('working on ', PORT));
-// REVIEW: POST route needs to parse the body passed in with the request. express.urlencoded() attaches "middleware" to do that
-// app.post('/articles', express.urlencoded(), function(request, response) {
-//   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
-//   console.log(request.body);
-//   response.send('Record posted to server!!');
-// });
+//REVIEW: POST route needs to parse the body passed in with the request. express.urlencoded() attaches "middleware" to do that
+app.post('/articles', express.urlencoded(), function(request, response) {
+  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  console.log(request.body);
+  response.send('Record posted to server!!');
+});
